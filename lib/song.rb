df40672b529d172e89205b6664e120fec306d8e2
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-03-06 20:09:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,6 +27,17 @@ class Song
       @@artists.uniq!
     end
 
+    def sefl.genre_count
+      genre_count = {}
+      @@genres.each do |genre|
+        if genre_count[genre]
+          genre_count[genre] =+1
+        else
+          genre_count[genre] = 1
+        end
+     end
+     genre_count
+
 
 
 end
