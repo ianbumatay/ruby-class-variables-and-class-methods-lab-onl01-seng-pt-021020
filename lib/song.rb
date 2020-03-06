fdef3ac1d4197ab38a14057767dda69a1ddd1087
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-03-06 19:54:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,16 @@
 class Song
 
+  attr_accessor :name, :artist, :genre
+
     @@count = 0
     @@artists = []
     @@genres = []
 
-
-   attr_accessor :name, :artist, :genre
-
-   def initialize(name, artist, genre)
+  def initialize(name, artist, genre)
       @name = name
       @artist = artist
       @genre = genre
-      @@count =+ 1
+      @@count =+1
       @@artists << artist
       @@genres << genre
    end
