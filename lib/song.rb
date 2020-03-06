a673b2f2889a701cc3e9f9f36bdf0865997c26d1
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-03-06 20:20:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,9 +39,18 @@ class Song
     genre_count
   end
 
+  def self.artist_count
+    artist_count = {}
+    @@artists each do |artist|
+      if artist_count[artist]
+        artsist_count[artist] +=1
+      else
+        artsist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
 
 
-
-
-
+  end
 end
